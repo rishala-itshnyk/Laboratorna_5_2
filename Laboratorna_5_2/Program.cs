7c97a5f06f81d3737d8674f125ce6da5e00ab9be
Rishala Itshnyk <rishala.itshnyk@gmail.com>
2023-11-06 09:55:05 +0200

    initial commit

---
+++ Laboratorna_5_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main()
+        {
+            double xp, xk, x, dx, eps, s = 0;
+            int n = 0;
+            Console.Write("xp = ");
+            xp = double.Parse(Console.ReadLine());
+            Console.Write("xk = ");
+            xk = double.Parse(Console.ReadLine());
+            Console.Write("dx = ");
+            dx = double.Parse(Console.ReadLine());
+            Console.Write("eps = ");
+            eps = double.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("|" + " x " + "|" + " log((1.0 + x)/(1.0-x)) " + "|" + " S " + "|" + " n " + "|");
+            Console.WriteLine("-------------------------------------------------");
+            x = xp;
+            while (x <= xk)
+            {
+                s = S(x, eps, ref n, s);
+                Console.WriteLine("|" + x.ToString("F2") + " |" + Math.Log((1.0 + x) / (1.0 - x)).ToString("F5") + " |" + s
+                    .ToString("F5") + " |" + n.ToString("F2") + " |");
+                x += dx;
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public static double S(double x, double eps, ref int n, double s)
+        {
+            n = 0;
+            double a = x;
+            s = a;
+            do
+            {
+                n++;
+                a = A(x, n, a);
+                s += a;
+            } while (Math.Abs(a) >= eps);
+
+            return s;
+        }
+
+        static double A(double x, int n, double a)
+        {
+            double R = 2.0 * (x * x) * ((2 * n - 1.0) / (2 * n + 1.0));
+            a *= R;
+            return a;
+        }
+    }
+}
---
+++ Program.Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Program.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            double x = 0.5;
+            double eps = 0.0001;
+            int n = 0;
+            double s = 0;
+            double expected = Math.Log((1 + x) / (1 - x));
+
+            // Act
+            double actual = Program.S(x, eps, ref n, s);
+        }
+    }
+}
